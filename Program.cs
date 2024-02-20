0bd9877ae6ed8042921332048d0bfcac4483d45e
Usuario <Usuario@CPD02-M12>
2024-02-20 12:18:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+            Console.WriteLine(2);
+            Console.WriteLine(3.5);
+            Console.WriteLine(2 + 2);
+            Console.WriteLine("O resultado é: " + (2 + 2));
+            Console.WriteLine(2 + 2 + " é o resultado");
+            Console.WriteLine(2 + 2 * 3);
+
+            Console.ReadKey();
+        }
+    }
+}
